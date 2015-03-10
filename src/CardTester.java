b6d5167f4d6f5cdd005d9bde83f594a4bd061b33
lucybrooks <lucybrooks@users.noreply.github.com>
2015-03-09 19:16:50 -0700

    Update CardTester.java

--- src/CardTester.java
+++ src/CardTester.java
@@ -7,7 +7,15 @@ public class CardTester {
 	 * The main method in this class checks the Card operations for consistency.
 	 *	@param args is not used.
 	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	public static void main(String[] args) 
+	{
+	Card card1 = new Card ("puppies", "star", 7);
+	Card card2 = new Card ("kittens", "star", 6);
+	Card card3 = new Card ("goldfish", "star", 9);
+	
+	card1.matches(card2);
+	card3.toString();
+	
+	
 	}
 }
